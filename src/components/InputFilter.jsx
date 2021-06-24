503601fe5076da83b619be1dd875e77735f599bc
Arlen Freitas <arlenfreitas1@gmail.com>
2021-06-24 17:24:35 -0300

    req 3

--- src/components/InputFilter.jsx
+++ src/components/InputFilter.jsx
@@ -2,7 +2,14 @@ import React, { useContext } from 'react';
 import context from '../context/context';
 
 function InputFilter() {
-  const { filterPlanetsByName } = useContext(context);
+  const { filterPlanetsByName,
+    filterByNumericValues,
+    filters,
+    filterButton,
+    value,
+    comparison,
+    column,
+  } = useContext(context);
 
   return (
     <form action="">
@@ -10,13 +17,51 @@ function InputFilter() {
         <legend>Filtrar por nome:</legend>
         <input
           type="text"
-          name="planet"
-          id="planet"
           data-testid="name-filter"
           onChange={ (ev) => {
             filterPlanetsByName(ev);
           } }
         />
+      </fieldset>
+      <fieldset>
+        <legend>Filtrar por valores</legend>
+        <select
+          name="column"
+          data-testid="column-filter"
+          onChange={ filterByNumericValues }
+          value={ column }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          name="comparison"
+          data-testid="comparison-filter"
+          onChange={ filterByNumericValues }
+          value={ comparison }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          name="number"
+          data-testid="value-filter"
+          onChange={ filterByNumericValues }
+          value={ value }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ filterButton }
+        >
+          Filtrar
+
+        </button>
       </fieldset>
     </form>
   );
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -6,6 +6,12 @@ import context from './context';
 function Provider({ children }) {
   const [data, setData] = useState([]);
   const [filterPlanets, setFilterPlanets] = useState([]);
+  const [filters, setFilters] = useState({
+    name: '',
+    value: 100000,
+    column: 'population',
+    comparison: 'maior que',
+  });
 
   const planetData = async () => {
     const planets = await getApi();
@@ -23,9 +29,35 @@ function Provider({ children }) {
     setFilterPlanets(filterData);
   };
 
+  const filterByNumericValues = ({ target: { name, value } }) => {
+    setFilters({
+      ...filters,
+      [name]: value,
+    });
+  };
+
+  const filterButton = () => {
+    const { number, column, comparison } = filters;
+
+    const filteredData = data.filter((planet) => {
+      const columnCompare = Number(planet[column]);
+      const valueCompare = Number(number);
+      if (comparison === 'maior que') {
+        return columnCompare > valueCompare;
+      }
+      if (comparison === 'menor que') {
+        return columnCompare < valueCompare;
+      }
+      return columnCompare === valueCompare;
+    });
+    setFilterPlanets(filteredData);
+  };
+
   const contextValue = {
     filterPlanets,
     filterPlanetsByName,
+    filterByNumericValues,
+    filterButton,
   };
 
   return (
